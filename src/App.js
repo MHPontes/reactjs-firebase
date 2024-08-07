35a3dc5dfe21346aaa65b52062951f6bfffe426d
MHPontes <mateushspontes@gmail.com>
2024-08-07 09:09:28 -0300

    configuracoes iniciais

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { db } from "./firebaseConnection";    // Importando o db para inserir dados no Firestore
+import { doc, setDoc } from "firebase/firestore";     // Importando o setDoc para inserir dados no Firestore
+import  "./App.css";
+
 
 function App() {
+const [titulo, setTitulo] = useState("");
+const [autor, setAutor] = useState("");
+
+
+  async function handleAdd() {       // Função para adicionar os dados no Firestore, async para aguardar a execução do setDoc e não travar a aplicação 
+    await setDoc(doc(db, "posts", "123456997712"), {     
+      titulo: titulo,
+      autor: autor
+    })
+    .then(() => {
+      console.log("Dados cadastrados com sucesso!");
+    })
+    .catch((error) => {
+      console.log("Erro ao cadastrar os dados: ", error);
+    });
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ReactJS + Firebase</h1>
+
+      <div className="container">
+        <label>Titulo:</label>
+        <textarea 
+        type="text" 
+        placeholder="Digite o título" 
+        value={titulo}
+        onChange={(e) => setTitulo(e.target.value)}  />    {/* onChange e disparado toda vez que um valor é salvo e.target pega o valor do input */} 
+
+        <label>Descrição:</label>
+        <input 
+        type="text" 
+        placeholder="Autor do post" 
+        value={autor}
+        onChange={(e) => setAutor(e.target.value)}  />
+
+        <button onClick={handleAdd}>Cadastrar</button>
+      </div>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/firebaseConnection.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {      // Configurações do Firebase, que são obtidas no momento da criação do projeto no Firebase
+
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  
+    authDomain: "curso-96cde.firebaseapp.com",
+  
+    projectId: "curso-96cde",
+  
+    storageBucket: "curso-96cde.appspot.com",
+  
+    messagingSenderId: "318928259455",
+  
+    appId: "1:318928259455:web:ef4bb98c9b95c6f653324a",
+  
+    measurementId: "G-94PJKS3455"
+  
+    
+  };
+
+const firebaseApp = initializeApp(firebaseConfig);   // Iniciando o Firebase com as configurações do firebaseConfig
+
+const db = getFirestore(firebaseApp);  // Iniciando o Firestore com as configurações do firebaseApp
+
+export { db };   // Exportando o db para ser utilizado em outros arquivos
